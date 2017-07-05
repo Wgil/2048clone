6e2004e3d3bf34b1a5cb07a86312ff86d37c9499
Warao Gil <wgil14@gmail.com>
2017-07-05 15:51:59 -0400

    - Upload project.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MoveDirection {
+	Left, Right, Up, Down
+}
+
+public class InputManager : MonoBehaviour {
+
+	private GameManager gm;
+
+	void Awake()
+	{
+		gm = GameObject.FindObjectOfType<GameManager> ();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			gm.Move(MoveDirection.Right);
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			gm.Move(MoveDirection.Left);
+		}
+		else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			gm.Move(MoveDirection.Up);
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			gm.Move(MoveDirection.Down);
+		}
+	}
+}
